5a956d35ab347b42143392a53724a86426e66291
João Bispo <jbispo@fe.up.pt>
2026-01-26 11:14:55 +0000

    [FortranParser] Updates code to most recent version of specs-java-libs

--- FortranParser/src/pt/up/fe/specs/fortran/parser/FortranNativeParser.java
+++ FortranParser/src/pt/up/fe/specs/fortran/parser/FortranNativeParser.java
@@ -103,7 +103,7 @@ public class FortranNativeParser {
 
 
         // Resolve filename (taking into account versioning)
-        var resourceWithVersion = LINUX_DUMPER.createResourceVersion("_" + LINUX_DUMPER.getVersion());
+        var resourceWithVersion = LINUX_DUMPER.createResourceVersion("_" + LINUX_DUMPER.version());
         var pluginFile = new File(TEMP_FOLDER.get(), resourceWithVersion.getFilename());
 
 
