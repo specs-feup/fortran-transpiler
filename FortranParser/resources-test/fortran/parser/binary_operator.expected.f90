e37512611f368ea024097a0f805cdd7c8ac42817
Onso37 <80353484+Onso37@users.noreply.github.com>
2026-02-10 21:35:09 +0100

    Simple binary operation test

---
+++ FortranParser/resources-test/fortran/parser/binary_operator.expected.f90
@@ -0,0 +1,5 @@
+PROGRAM sum_example
+    integer :: result
+
+    result = 5 + 10
+END PROGRAM sum_example
---
+++ FortranParser/resources-test/fortran/parser/binary_operator.f90
@@ -0,0 +1,5 @@
+program sum_example
+    integer :: result
+
+    result = 5 + 10
+end program sum_example
