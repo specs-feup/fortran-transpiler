df0f76a80dc4e5c545caa1cecf4a14b969c068c9
Onso37 <80353484+Onso37@users.noreply.github.com>
2026-02-11 11:48:52 +0100

    Create field for operator kind

--- FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/expr/BinaryOperator.java
+++ FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/expr/BinaryOperator.java
@@ -1,11 +1,16 @@
 package pt.up.fe.specs.fortran.ast.nodes.expr;
 
+import org.suikasoft.jOptions.Datakey.DataKey;
+import org.suikasoft.jOptions.Datakey.KeyFactory;
 import org.suikasoft.jOptions.Interfaces.DataStore;
 import pt.up.fe.specs.fortran.ast.nodes.FortranNode;
+import pt.up.fe.specs.fortran.ast.nodes.expr.enums.BinaryOperatorKind;
 
 import java.util.Collection;
 
 public class BinaryOperator extends Expr {
+
+    public final static DataKey<BinaryOperatorKind> OP = KeyFactory.enumeration("op", BinaryOperatorKind.class);
 
     public BinaryOperator(DataStore data, Collection<? extends FortranNode> children) {
         super(data, children);
@@ -27,12 +32,16 @@ public class BinaryOperator extends Expr {
         return (Expr) setChild(1, newRhs);
     }
 
+    public BinaryOperatorKind getOp() {
+        return get(OP);
+    }
+
     @Override
     public String getCode() {
 
         return getLhs().getCode() +
 
-                //TODO append operator
+                " " + get(OP).getOpString() + " " +
 
                 getRhs().getCode();
     }
