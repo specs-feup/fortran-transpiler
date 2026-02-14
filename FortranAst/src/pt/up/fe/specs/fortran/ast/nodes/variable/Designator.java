9b1e8efa15b099203e62add7df7d69cd3b20d988
Onso37 <80353484+Onso37@users.noreply.github.com>
2026-02-14 11:59:43 +0100

    Make Designator extend Expr and delete Variable

--- FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/variable/Designator.java
+++ FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/variable/Designator.java
@@ -2,10 +2,11 @@ package pt.up.fe.specs.fortran.ast.nodes.variable;
 
 import org.suikasoft.jOptions.Interfaces.DataStore;
 import pt.up.fe.specs.fortran.ast.nodes.FortranNode;
+import pt.up.fe.specs.fortran.ast.nodes.expr.Expr;
 
 import java.util.Collection;
 
-public class Designator extends Variable{
+public class Designator extends Expr {
     public Designator(DataStore data, Collection<? extends FortranNode> children) {
         super(data, children);
     }
--- FortranParser/resources-test/fortran/parser/binary_operator.expected.f90
+++ FortranParser/resources-test/fortran/parser/binary_operator.expected.f90
@@ -1,18 +1,18 @@
 PROGRAM test_exprs
   logical :: l
-  integer :: i
-  l = 0 < 1
+  integer :: i, a = 1, b = 1
+  l = 0 < a
   l = 2 <= 3
-  l = 4 > 5
+  l = b > 5
   l = 6 >= 7
-  l = 8 == 9
+  l = a == b
   l = 0 /= 1
 
-  i = 0 + 1
-  i = 2 - 3
+  i = a + 1
+  i = a - b
   i = 4 * 5
-  i = 6 / 7
+  i = 6 / b
 
-  i = 0 + 1 - 3 * 5 / 2 + 7
+  i = 0 + 1 - b * 5 / a + 7
 
 END PROGRAM test_exprs
